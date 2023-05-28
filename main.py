34fdead4c8ec846a2502648cce0973091bcb2a91
yrqUni <yrqUni@gmail.com>
2023-05-28 16:45:26 +0800

    v0.0.1

--- main.py
+++ main.py
@@ -205,7 +205,8 @@ def main():
                     _all_to_consume = (log_time)/(((epoch*len_train_dataloader)+stepInEp)/(args.num_train_epochs*len_train_dataloader))
                     _estimated_to_consume = ((log_time)/(((epoch*len_train_dataloader)+stepInEp)/(args.num_train_epochs*len_train_dataloader)))*(1-(((epoch*len_train_dataloader)+stepInEp)/(args.num_train_epochs*len_train_dataloader)))
                     print_rank_0(f"epoch {epoch} part {P}/{len(os.listdir(args.data_sample_input_path))} stepInEp {stepInEp} train loss {_loss}, log_step {_log_step}, speed {_speed}, train schedule {_train_schedule}, all to consume {_all_to_consume}, estimated to consume {_estimated_to_consume}", args.global_rank)
-                    just_show(reconstructed_pixel_values,sample,patch_size,args.per_var_patch_side,args.data_output_path)
+                    if args.global_rank==0:
+                        just_show(reconstructed_pixel_values,sample,patch_size,args.per_var_patch_side,args.data_output_path)
                     training_step_losses = []
                 if stepInEp%args.save_step == 0 and args.global_rank == 0 and args.ckpt_output_dir is not None:
                     save_hf_format(model, args)
